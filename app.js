601eb702cd73b95d1b65f6aeced8da4a3350e09e
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-05-27 02:34:36 -0400

    ADD | Some fun stuff like sounds and animations

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ class App {
 
     // dom elements
     this.$form = document.querySelector(".rating-card");
-    this.$thankYouCard = document.querySelector(".thank-you-card");
+    this.$icon = document.querySelector(".rating-card__icon-container");
     this.$buttons = document.querySelectorAll(".js-rating-card__button");
     this.$submitButton = document.querySelector(".rating-card__button--submit");
     this.$result = document.querySelector(".thank-you-card__chip");
+    this.$thankYouCard = document.querySelector(".thank-you-card");
+    this.$resetLink = document.querySelector(".thank-you-card__h3");
+    this.$sounds = document.querySelectorAll(".audio");
+
+    window.onload = () => {
+      this.animate();
+    }
     this.handleEventListeners();
   }
 
@@ -38,11 +45,52 @@ class App {
         this.$form.classList.add("hidden");
         this.$result.textContent = `You selected ${this.rating} out of 5`;
         this.$thankYouCard.classList.remove("hidden");
+        this.$thankYouCard.classList.remove("right");
+        this.playSound("submit");
       } else {
         console.log("not submit!");
       }
     })
+
+    // reset the form
+    this.$resetLink.addEventListener("click", () => {
+      this.rating = 0;
+      this.$thankYouCard.classList.add("hidden");
+      this.$form.classList.remove("hidden");
+      this.$buttons.forEach(button => {
+        button.classList.remove("active");
+      })
+      this.playSound("reset");
+    })
+  }
+
+  reset() {
+    this.$buttons.forEach((button, i) => {
+      button.classList.remove("flick");
+    })
+  }
+
+  animate() {
+    this.$icon.classList.add("spin");
+
+    this.$buttons.forEach((button, i) => {
+      button.style.transitionDelay = `${i * 125}ms`;
+      button.classList.add("flick");
+    })
+  }
+
+  playSound(action, sounds = this.$sounds) {
+    console.log(sounds);
+    if (action) {
+      sounds.forEach(sound => {
+        if (sound.dataset.action === action) {
+          sound.currentTime = 0;
+          sound.play();
+        }
+      })
+    }
   }
 }
+
 
 new App();
